3bb2109341902e266e19728d89f0d8b5a9eb59ea
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-05-09 17:57:57 +0200

    Add collapse

--- src/js/01-consant.js
+++ src/js/01-consant.js
@@ -1,27 +1,31 @@
 'use strict';
-
 const menudesing = document.querySelector (".js-formcontainer-desing");
 const menufield = document.querySelector (".js-formcontainer-field");
 const menushare = document.querySelector (".js-formcontainer-share")
 const field = document.querySelector (".js-field");
 const design = document.querySelector (".js-desing");
-const share = document.querySelector ("js-share");
-const iconmenudesing = document.querySelector (".fa-chevron-up");
-
+const share = document.querySelector (".js-share")
+const iconmenu = document.querySelector (".fa-chevron-up");
+const iconmenu2 = document.querySelector (".js-2");
+const iconmenu3 = document.querySelector (".js-3");
 
 function expanddesing (){
 menudesing.classList.toggle('hidden')
 iconmenu.classList.toggle('jsmenu')
-}
-function expandfield (){
-    menufield.classList.toggle('hidden')
-}
-function expandshare (){
-menushare.classList.toggle('hidden')
-}
-    
+};
+
+function expandfield () {
+menufield.classList.toggle ('hidden')
+iconmenu2.classList.toggle('jsmenu')
+};
+
+function expandshare () {
+    menushare.classList.toggle ('hidden')
+    iconmenu3.classList.toggle('jsmenu')
+    };
 
 design.addEventListener ('click', expanddesing)
 field.addEventListener ('click', expandfield)
 share.addEventListener ('click', expandshare)
+
 
