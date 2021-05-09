086c03876607405d4464246e4b4e851bd66c4726
EliaAR <alonsoramos.elia@gmail.com>
2021-05-09 13:22:38 +0200

    add collapse proposal

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,54 @@
-'use strict';
+//'use strict';
 
-console.log('>> Ready :)');
+//Eventos para desplegar secciones con requisitos
+let selectionDesign = document.querySelector(".js-design");
+let selectionDesignButton = document.querySelector(".js-designButton");
+function collapseValidationDesign() {
+  selectionDesign.classList.toggle("none");
+  selectionDesignButton.classList.toggle("down");
+}
+selectionDesignButton.addEventListener("click", collapseValidationDesign);
+
+let selectionFill = document.querySelector(".js-fill");
+let selectionFillButton = document.querySelector(".js-fillButton");
+let messageFill = document.querySelector(".js-tooltipFill");
+function collapseValidationFill(e) {
+  if (validationDesign === false) {
+    messageFill.classList.toggle("none");
+    messageFill.style.top = e.clientY + "px";
+    messageFill.style.left = e.clientX + "px";
+    setTimeout(function () {
+      messageFill.classList.toggle("none");
+    }, 3000);
+  } else {
+    selectionFill.classList.toggle("none");
+    selectionFillButton.classList.toggle("down");
+  }
+}
+selectionFillButton.addEventListener("click", validateDesignSection);
+selectionFillButton.addEventListener("click", collapseValidationFill);
+
+let selectionShare = document.querySelector(".js-share");
+let selectionShareButton = document.querySelector(".js-shareButton");
+let messageShare = document.querySelector(".js-tooltipShare");
+function collapseValidationShare() {
+  if (validationDesign === false || validationFill === false) {
+    messageShare.classList.toggle("none");
+  } else {
+    selectionShare.classList.toggle("none");
+    selectionShareButton.classList.toggle("down");
+  }
+}
+selectionShareButton.addEventListener("click", collapseValidationShare);
+
+//Validation
+let validationDesign = false;
+function validateDesignSection() {
+  let inputsDesign = document.querySelectorAll('input[name="colors"]');
+  inputsDesign.forEach((e) => {
+    if (e.checked) {
+      validationDesign = true;
+    }
+  });
+}
+validateDesignSection();
