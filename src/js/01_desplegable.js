03721e2d99f3fbb94b9a8bbbfab5fe380aa17389
Marcela Chuqi <81633985+marcelachuqi@users.noreply.github.com>
2021-05-10 00:33:19 +0200

    add marcela branch

--- src/js/01_desplegable.js
+++ src/js/01_desplegable.js
@@ -11,23 +11,40 @@
 
 const scrollUp = document.querySelector(".fas");
 console.log("hola");
-
 const scrollDown = scrollUp.classList.add("fas_js");
 
 function upsidedownIcon() {
-  return scrollUp.classList.add("fas_js");
+  // console.log("Click");
+  // scrollUp.classList.add("fas_js");
+  scrollUp.Target.classList.toggle("fas_js");
 }
 scrollUp.addEventListener("click", upsidedownIcon);
 
-//
+// const fillform = document.querySelector(".fill");
 
-const fillform = document.querySelector(".fill");
+// const fillformHide = fillform.classList.add("fill_js");
 
-const fillformHide = fillform.classList.add("fill_js");
+// function formhideevent() {
+//    if (fillform.classList.has('fill')) {
+//     fillform.classList.add('fill_js');
+//    }
 
-function formhideevent() {
-   if (fillform.classList.has('fill')) {
-    fillform.classList.add('fill_js');
-   }
+// scrollUp.addEventListener("click", formhideevent);
 
-scrollUp.addEventListener("click", formhideevent);
+const nameProfile = document.querySelector(".name_js");
+const nameContact = document.querySelector(".fill__contact-name");
+const nameValue = nameContact.value;
+
+nameProfile.innerHTML = nameContact.addEventListener(
+  "change",
+  nameProfileaction
+);
+
+function nameProfileaction() {
+  // nameContact.classList.toggle("fill__contact-name");
+  if (nameProfile.classList.contains("fill__contact")) {
+    nameProfile.classList.remove("fill__contact-name");
+  } else {
+    nameProfile === nameValue;
+  }
+}
