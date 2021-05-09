c46d6ca017a03965528571f3423454f610aaece8
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-05-09 14:24:31 +0200

    Add icon expanded

--- src/js/01-consant.js
+++ src/js/01-consant.js
@@ -2,11 +2,12 @@
 
 const menu = document.querySelector (".js-formcontainer-desing");
 const design = document.querySelector (".js-desing");
+const iconmenu = document.querySelector (".fa-chevron-up");
 
 function expand (){
 menu.classList.toggle('hidden')
+iconmenu.classList.toggle('jsmenu')
 }
 
 design.addEventListener ('click', expand)
 
-console.log ('Hola');
