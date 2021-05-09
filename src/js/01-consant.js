abd1981fca24a45d9375532808f9d1e46f18fee7
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-05-09 19:23:49 +0200

    Test value form

--- src/js/01-consant.js
+++ src/js/01-consant.js
@@ -11,6 +11,12 @@ const iconmenu = document.querySelector (".fa-chevron-up");
 const iconmenu2 = document.querySelector (".js-2");
 const iconmenu3 = document.querySelector (".js-3");
 
+
 //PROFILE DESING//
+const nameProfile = document.querySelector (".js-nameprofile");
 
+const inputProfile = document.querySelector (".js-contact");
+ const nameSubname = document.querySelector (".js-formnamesubname")
+ 
 
+console.log (nameProfile);
---
+++ src/js/03-profile-field.js
@@ -0,0 +1,8 @@
+function word() {
+    nameProfile.innerHTML = inputProfile.value
+
+}
+
+nameSubname.addEventListener ('change', word)
+
+console.log (inputProfile.value)
