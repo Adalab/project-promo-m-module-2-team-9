b430966a2a4cd51e008812cf9f967e797867df7b
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-05-09 13:41:29 +0200

    Add desgin expand

--- src/js/01-consant.js
+++ src/js/01-consant.js
@@ -1,3 +1,12 @@
 'use strict';
 
+const menu = document.querySelector (".js-formcontainer-desing");
+const design = document.querySelector (".design");
+
+function expand (){
+menu.classList.toggle('hidden')
+}
+
+design.addEventListener ('click', expand)
+
 console.log ('Hola');
