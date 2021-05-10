d141b935b7916b56fa94e8086a86b473b557c0ce
Marcela Chuqi <81633985+marcelachuqi@users.noreply.github.com>
2021-05-11 00:01:56 +0200

    add event to input name

--- src/js/01_events.js
+++ src/js/01_events.js
@@ -25,59 +25,22 @@
 // // const fillformHide = fillform.classList.add("fill_js");
 
 // // function formhideevent() {
-// //    if (fillform.classList.has('fill')) {
-// //     fillform.classList.add('fill_js');
-// //    }
+// // //    if (fillform.classList.has('fill')) {
+// // //     fillform.classList.add('fill_js');
+// // //    }
 
-// // scrollUp.addEventListener("click", formhideevent);
-
+// // // scrollUp.addEventListener("click", formhideevent);
+//////////////////////////////
 const form = document.querySelector(".form");
-const nameProfile = document.querySelector(".profile_name");
+const nameProfile = document.querySelector(".profile__name");
 const nameContact = document.querySelector(".fill__contact-name");
 const nameValue = nameContact.value;
 
 function handleProfileChange(ev) {
   const inputChanged = ev.target;
   const changedValue = inputChanged.value;
-
-  if (nameProfile === "Nombre Apellido") {
-    nameProfile.innerHTML = nameValue;
+  if (inputChanged.name === "name") {
+    nameProfile.innerHTML = changedValue;
   }
-
-  console.log("Target", ev.target);
-  console.log("Current Target", ev.currentTarget);
 }
-
 form.addEventListener("change", handleProfileChange);
-
-// /* Form */
-// /*
-// const inputName = document.querySelector('.js-name');
-// const handleChangeName = (ev) => {
-//     console.log( 'Target', ev.target );
-//     console.log( 'Current Target', ev.currentTarget );
-// };
-// inputName.addEventListener('change', handleChangeName);
-// */
-
-// const form = document.querySelector('.js-form');
-// const previewNameElement = document.querySelector('.js-preview-name');
-
-// function handleChangeForm(ev) {
-//     const inputChanged = ev.target;
-//     const changedValue = inputChanged.value;
-
-//     if( inputChanged.name === 'name' ) {
-//         previewNameElement.innerHTML = changedValue;
-//     }
-//     else if( inputChanged.name === 'linkedin' ) {
-//         previewLinkedinElement.href = changedValue;
-//     }
-
-//     console.log( 'Target', ev.target );
-//     console.log( 'Current Target', ev.currentTarget );
-// }
-
-// form.addEventListener('change', handleChangeForm);
-
-// console.log('>> Form Ready :)');
