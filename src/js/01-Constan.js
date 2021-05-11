ca69580b5ea506405e5fa28eb6fcad33dd7f7ffb
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-05-11 17:28:05 +0200

    Add js

---
+++ src/js/01-Constan.js
@@ -0,0 +1,15 @@
+//COLLAPSE DESIGN//
+
+let selectionDesign = document.querySelector(".js-design");
+let selectionDesignButton = document.querySelector(".js-designButton");
+
+//COLLAPSE FILL//
+let selectionFill = document.querySelector(".js-fill");
+let selectionFillButton = document.querySelector(".js-fillButton");
+let messageFill = document.querySelector(".js-tooltipFill");
+
+
+//COLLAPSE SHARE//
+let selectionShare = document.querySelector(".js-share");
+let selectionShareButton = document.querySelector(".js-shareButton");
+//let messageShare = document.querySelector(".js-tooltipShare");
