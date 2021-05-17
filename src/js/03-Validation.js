618a55985bd06fc2dc1bd31e4fb1af742e1a707f
Marcela Chuqi <81633985+marcelachuqi@users.noreply.github.com>
2021-05-17 13:17:37 +0200

    change marging bottom

--- src/js/03-Validation.js
+++ src/js/03-Validation.js
@@ -1,7 +1,7 @@
 //ValidationDesing//
 let validationDesign = false;
 function validateDesignSection() {
-  let inputsDesign = document.querySelectorAll('input[name="colors"]');
+  let inputsDesign = document.querySelectorAll('input[name="palette"]');
   inputsDesign.forEach((e) => {
     if (e.checked) {
       validationDesign = true;
