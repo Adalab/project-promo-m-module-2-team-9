896f06ae64eb952088810e8edfb060c6c1044d6b
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-05-17 20:06:55 +0200

    Form fill

--- src/js/04-Profile-card.js
+++ src/js/04-Profile-card.js
@@ -1,16 +1,37 @@
-function handelChangeform(ev) {
-  const inputChanged = ev.target;
-  const changedValue = inputChanged.value;
-  if (inputChanged.name === "fullname") {
-    previewNameElemento.innerHTML = changedValue;
-  } else if (inputChanged.name === "role") {
-    previewRoleElemento.innerHTML = changedValue;
-  }
-  /* else if( inputChanged.name === 'mailto:saly@hotmail.com' ) {
+function changedForm(ev) {
+  const inputNameChange = ev.target.name;
+  const changedValue = ev.target.value;
+
+  data[inputNameChange] = changedValue;
+  console.log(data);
+}
+
+function refreshData() {
+  previewNameElemento.innerHTML =
+    data.name === "" ? "Nombre apellidos" : data.name;
+  previewRoleElemento.innerHTML =
+    data.job === "" ? "Front-end developer" : data.job;
+  previewPhoneElement.href += data.phone;
+  previewMailElement.href += data.email;
+  previewLinkedinElement.href = data.linkedin;
+  previewGithubElement.href = data.github;
+}
+
+function handleChangeForm(ev) {
+  // 1. Cojo el nuevo valor y lo guardo en una variable
+
+  changedForm(ev);
+
+  // 2. Re-pinto la tarjeta de preview
+
+  refreshData();
+}
+
+/* else if( inputChanged.name === 'mailto:saly@hotmail.com' ) {
         previewMailElemento.href = changedValue;
     }
     else if( inputChanged.name === 'mailto:saly@hotmail.com' ) {
         previewLinkedinElemento.href = changedValue;
     } */
-}
-form.addEventListener("keyup", handelChangeform);
+
+form.addEventListener("keyup", handleChangeForm);
