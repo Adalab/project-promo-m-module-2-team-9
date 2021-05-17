21decdbd98fa3b77585429857d208aa95560c443
Marcela Chuqi <81633985+marcelachuqi@users.noreply.github.com>
2021-05-17 13:21:42 +0200

    change main to object

--- src/js/04-Profile-card.js
+++ src/js/04-Profile-card.js
@@ -1,17 +1,16 @@
-function handelChangeform (ev)  {
-    const inputChanged = ev.target;
-    const changedValue = inputChanged.value;
-    if ( inputChanged.name === 'fullname') {
-        previewNameElemento.innerHTML = changedValue;
-    }
-    else if ( inputChanged.name === 'role' ) {
-        previewRoleElemento.innerHTML = changedValue;
-    }
-   /* else if( inputChanged.name === 'mailto:saly@hotmail.com' ) {
+function handelChangeform(ev) {
+  const inputChanged = ev.target;
+  const changedValue = inputChanged.value;
+  if (inputChanged.name === "fullname") {
+    previewNameElemento.innerHTML = changedValue;
+  } else if (inputChanged.name === "role") {
+    previewRoleElemento.innerHTML = changedValue;
+  }
+  /* else if( inputChanged.name === 'mailto:saly@hotmail.com' ) {
         previewMailElemento.href = changedValue;
     }
     else if( inputChanged.name === 'mailto:saly@hotmail.com' ) {
         previewLinkedinElemento.href = changedValue;
     } */
 }
-    form.addEventListener('keyup', handelChangeform)
+form.addEventListener("keyup", handelChangeform);
