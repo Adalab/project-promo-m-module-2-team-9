9f6375b09da79b9bdabd4c34bc702053162943e1
Marcela Chuqi <81633985+marcelachuqi@users.noreply.github.com>
2021-05-17 11:59:20 +0200

    add events profile card

--- src/js/04-Profile-card.js
+++ src/js/04-Profile-card.js
@@ -1,17 +1,22 @@
-function handelChangeform (ev)  {
-    const inputChanged = ev.target;
-    const changedValue = inputChanged.value;
-    if ( inputChanged.name === 'fullname') {
-        previewNameElemento.innerHTML = changedValue;
-    }
-    else if ( inputChanged.name === 'role' ) {
-        previewRoleElemento.innerHTML = changedValue;
-    }
-   /* else if( inputChanged.name === 'mailto:saly@hotmail.com' ) {
-        previewMailElemento.href = changedValue;
-    }
-    else if( inputChanged.name === 'mailto:saly@hotmail.com' ) {
-        previewLinkedinElemento.href = changedValue;
-    } */
+const previewemailElemento = document.querySelector(".email_js");
+const previewphoneElemento = document.querySelector(".mobile_js");
+const previewLinkedinElemento = document.querySelector(".linkedin_js");
+const previewgithubElemento = document.querySelector(".github_js");
+
+function handelChangeform(ev) {
+  const inputChanged = ev.target;
+  const changedValue = inputChanged.value;
+  if (inputChanged.name === "fullname") {
+    previewNameElemento.innerHTML = changedValue;
+  }
+  if (inputChanged.name === "role") {
+    previewRoleElemento.innerHTML = changedValue;
+  }
+  if (inputChanged.name === "email") {
+    previewemailElemento.href = changedValue;
+  }
+  if (inputChanged.name === "phone") {
+    previewphoneElemento.href = changedValue;
+  }
 }
-    form.addEventListener('keyup', handelChangeform)
+form.addEventListener("keyup", handelChangeform);
