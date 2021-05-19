66e634d4db67e09a04e35354c9dd907817341037
marialemon <maria.nc@hotmail.es>
2021-05-19 12:41:39 +0200

    work with fetch

--- src/js/06-send.js
+++ src/js/06-send.js
@@ -1,15 +1,30 @@
 //Lo que ha hecho Iván en clase
 
 const createButton = document.querySelector(".js-create-card");
+const responseElement = document.querySelector(".js-response");
 
 function handleClickCreate(ev) {
   ev.preventDefault();
 
-  fetch("https://awesome-profile-cards.herokuapp.com/", {
+  fetch("https://awesome-profile-cards.herokuapp.com/card", {
     method: "POST",
     mode: "cors", //esto es para que el servidor tenga en cuenta que pueden llegar peticiones de otros sitios
     body: JSON.stringify(data),
-  });
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.success === false) {
+        responseElement.innerHTML = "Tienes que rellenar los campos";
+        responseElement.classList.remove("none");
+      } else {
+        responseElement.innerHTML = `<a href='${data.cardURL}'>Dirección url</a>`;
+        responseElement.remove("none");
+      }
+    })
+    .catch(() => {
+      responseElement.innerHTML = "Inténtalo más tarde.";
+      responseElement.remove("none");
+    });
 }
 
 createButton.addEventListener("click", handleClickCreate);
